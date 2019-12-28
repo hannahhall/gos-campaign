50c5bd4baa0a685a69a1915665a0792f93514a11
Hannah Hall <hannahhall@Hannahs-MacBook-Pro-2.local>
2019-12-28 15:44:52 -0600

    initial files

---
+++ campaign_manager/gos/models/spell.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Spell(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, default='')
+    description = models.TextField()
+    level = models.IntegerField()
+    casting_time = models.CharField()
+    range = models.CharField(max_length=20)
+    components = models.CharField(max_length=20, blank=True)
+    duration = models.CharField(max=30)
+    school = models.CharField(max=50)
+    attack_save = models.CharField(max=100)
+    damage_effect = models.CharField(max=100)
+    higher_levels = models.CharField(max=255)
+    
+    class Meta:
+        ordering = ['name']
